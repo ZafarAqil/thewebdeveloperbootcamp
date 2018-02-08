5bd9b5fc8b07629427e0737b65954140149e94d5
ZafarAqil <zafaraqil14@gmail.com>
2018-02-08 18:51:42 +0530

    added some js files

---
+++ javascript/age_calculator/script.js
@@ -0,0 +1,5 @@
+var age = prompt("What's your age?");
+
+age1 = age*365.5
+
+alert("You are " + age1 + " days old");
---
+++ javascript/js_stalker_exercise/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What's your First Name?");
+var lastName = prompt("What's your Last Name?");
+var age = prompt("What's your Age?");
+
+console.log("Your Full Name is " + firstName + " " + lastName);
+console.log("Age" +" " +age);
